0f42112a9bcaba325f6defc6b5ef37b177f52e52
GN <laughswith@gmail.com>
2023-12-21 15:10:11 -0800

    initial commit

---
+++ src/Assignments/LambdaLab.js
@@ -0,0 +1,76 @@
+// Lambda Lab Exercises
+
+// Exercise 1: ** Imports **
+// Consider if your Lambda needs imports (e.g., access to database). If database access is needed, you can import pg with the following code:
+const { Client } = require("pg");
+
+// Exercise 2: ** Handler function **
+// All Lambdas need to handle the request and response. Here is starter code that follows the import statement(s):
+
+//handler function wrapper
+export async function handler(event, context, callback) {
+  const data = JSON.parse(event.body);
+  console.log(data); //let's assume data.provider stores the provider organization's name
+
+  return "success"; //return statement
+}
+// - The event parameter is an object that allows us to get the data sent in the request.
+// - The context parameter is an object that holds information about the AWS Lambda environment.
+// - The callback parameter is a function that returns the result of the Lambda.
+// - JSON is needed to parse the data we get from the UI.
+// - It is always a good idea to log the data to check the data we get from the UI.
+
+// Exercise 3. ** Database Connection **
+// If the Lambda must connect with the database to run queries, you will need code to connect and to disconnect inside of the handler function. Here is starter code for the new Client instance of pg for the Lambda to connect to:
+
+//Connect to PostgresDB
+const client = new Client({
+  user: process.env.MOCK_USER,
+  host: process.env.MOCK_HOST,
+  database: process.env.MOCK_DB_NAME,
+  password: process.env.MOCK_PASSWORD,
+  port: process.env.MOCK_PORT,
+});
+
+await client.connect();
+
+// Exercise 4: ** Database Disconnection **
+// Disconnecting from the database is one line of code just above the return statement in the handler function:
+
+await client.end();
+
+// Exercise 5: ** Response Handling **
+// All Lambdas need a response for the callback parameter in the handler function to execute. Here is default code to start with:
+
+const response = {
+  statusCode: 200,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+  },
+  body: "success", //customize this value as needed so the UI can get data stored here; important: if sending results from a database query, be sure to execute JSON.stringify()
+};
+callback(null, response);
+
+// Exercise 6. ** Custom Code **.
+// Between the Database Connection code and the Response Handling code is the custom code you craft, the code you want the Lambda to execute. Here is starter code to see how PostgreSQL queries in JS look like, assuming the provider name was sent from the UI and stored in data.provider:
+
+const text = "SELECT * FROM provider_participant_intake WHERE provider = $1";
+
+const values = [data.provider];
+
+const providerIntakes = await client.query(text, values);
+console.log(`${data.provider} intakes`, providerIntakes);
+// - it's always a good idea to log the result of the query throughout your handler function.
+// - since logs can accumulate as you build custom code in your handler function, it's also a good idea to put a note about what is logged, thus the string `${data.provider}'s intakes`
+
+// Exercise 7: ** Accessing the Serverless Offline Server
+// Before building any custom code, let's check that your Lambda setup and code actually works when a request is sent from Postman.
+
+// 1. Open the VS Code Terminal for this project and enter this command to start your local server: sls offline
+// Terminal will print: Server ready: http://localhost:3000 🚀
+
+// 2. Open Postman, then make a request at http://localhost:3000/<<lambda file name in all lowercase>> with raw JSON in its body: {"provider": "Future Works"}
+// The results should print in console.
+
+// 3. Every time you refactor your code, in VS Code Terminal, stop the server by pressing: Control + C. Then restart the server to test your refactored code: sls offline. Then test the request in Postman.
---
+++ src/Assignments/LambdaLab.md
@@ -0,0 +1,62 @@
+## Lambda Lab Activity
+
+Purpose: practice applying tools learned in the Product Lambda Diagramming and Shadowing a New Lambda Build workshop.
+
+## Refactoring and/or Building a New Lambda Summary
+
+1. The Serverless framework for AWS allows us to create code for AWS configurations and deployments of Lambda functions automatically.
+
+2. Key files for custom code are in our:
+
+   - JSON (secrets)
+   - YML (references secrets; sets up runtime, lambda event trigger paths, etc)
+   - Lambda files (handler function for requests and responses)
+
+3. Lambda file names should correspond with the lambda event trigger path in YML file. This will allow Postman calls to work.
+
+## JSON File
+
+Your existing **secrets.json** file needs one more key-value pair.
+
+JSON Exercise 1: In the **secrets.json** file, please add within the curly braces {} a new key-value pair (order does not matter; it can be listed first before all the mock key-value pairs):
+
+"SERVICE": "resourcefull-query-practice"
+
+## YML File
+
+The **serverless.yml** file's _functions_ section needs your new Lambda file name and its directory. Consider the following items for naming the Lambda and creating the directory.
+
+A Lambda file name has 2 parts:
+
+1. Method: is the Lambda getting, adding, updating, or deleting data?
+2. Feature: what is the Lambda getting, adding, updating or deleting?
+   Note: What is most important is if the name makes sense to another developer.
+
+Examples:
+
+1. getAllProviders - this Lambda gets all providers' information to display in a table
+2. addFavorites - this Lambda allows a user to add a provider as a favorite
+3. checkUserExists - this Lambda checks if a username (email address) already exists
+4. updateProviderProfile - this Lambda update's the provider's visibility in our listings
+5. resetTempPassword - this Lambda resets a user's temporary password
+6. sendWelcomeEmail - this Lambda sends a first-time user a welcome email
+7. deleteAccount - this Lambda allows a user to remove their account
+
+YML Exercise 1: For the Reports tab in the ResourceFull Provider App, can you think of an appropriate name for a new Lambda?
+
+YML Exercise 2: Since the Reports tab does not exist in the app yet, and all the other tabs have its own directory, create a folder within the src folder for this new directory.
+
+YML Exercise 3: In the folder you just created to house all Lambdas needed for this new Reports tab, create a JS file with the new Lambda's name from YML Exercise 1.
+
+YML Exercise 4: In the **serverless.yml** file, scroll to the **functions** section to add your Lambda file name and its directory in the applicable places, deleting placeholder punctuation <<>> as well.
+
+YML Exercise 5: In the **serverless.yml** file's **functions** section, the *method* for your function may be DIFFERENT from the naming of your lambda! For example, *getProviderProfile* may actually have a *post* method because the request posts the provider organization's name from the UI to the Lambda. So consider whether your UI sends data to your Lambda, because our Lambdas usually only have two methods: *get* or *post*
+   method: get
+   OR
+   method: post
+
+YML Exercise 6: You can now comment everything in for the **functions** section; keep ADMIN commented out.
+
+## Lambda File
+
+Since markdown punctuation can impact copy and paste within code files, please open LambdaLab.js to get starter code to build your custom code for the Lambda's handler function.
