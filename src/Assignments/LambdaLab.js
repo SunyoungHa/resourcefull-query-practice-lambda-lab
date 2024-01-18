21d7103c7cbe588b8473acfdfbf64e909d161193
GN <laughswith@gmail.com>
2024-01-18 14:16:35 -0800

    added the note to install serverless globally

--- src/Assignments/LambdaLab.js
+++ src/Assignments/LambdaLab.js
@@ -67,13 +67,17 @@ console.log(`${data.provider} intakes`, providerIntakes);
 // Exercise 7: ** Accessing the Serverless Offline Server
 // Before building any custom code, let's check that your Lambda setup and code actually works when a request is sent from Postman.
 
-// 1. Open the VS Code Terminal for this project and enter this command to start your local server: sls offline
+// 1. First, open the Terminal that's in your Dock. We will need to install serverless globally by running this command: npm install -g serverless
+
+// 2. Open the VS Code Terminal for this project and enter this command to start your local server: sls offline
 // Terminal will print: Server ready: http://localhost:3000 🚀
 
-// 2. Open Postman, then make a request at http://localhost:3000/<<lambda file name in all lowercase>> with raw JSON in its body: {"provider": "Future Works"}
+// 3. Open Postman, then make a request at http://localhost:3000/<<lambda file name in all lowercase>> with raw JSON in its body: {"provider": "Future Works"}
 // The results should print in console.
 
-// 3. Every time you refactor your code, in VS Code Terminal, stop the server by pressing: Control + C. Then restart the server to test your refactored code: sls offline. Then test the request in Postman.
+// 4. Every time you refactor your code, in VS Code Terminal, stop the server by pressing: Control + C. Then restart the server to test your refactored code: sls offline. Then test the request in Postman.
+
+// 5. Remember to stop your server with Control + C when it's not needed.
 
 // Exercise 8: From the Pseudocoding Databases with Product Querying Practice Lab Activity for the Reports tab in the ResourceFull Provider App, try refactoring 3 queries to insert your own PostgreSQL in JS queries in the Lambda file you created. Make sure you console.log() every query result. Then test in Postman to see if you get the appropriate responses logged in VS Code Terminal!
 
